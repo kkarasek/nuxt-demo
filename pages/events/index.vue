16e53cd3225841ce48806800e502aad036ae5f5c
Jakub Karasek <jkarasek@Jakubs-MacBook-Air.local>
2023-10-21 13:23:58 +0200

    add layouts

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,4 +1,18 @@
+<script setup lang="ts">
+  useHead({
+    title: 'Events',
+    htmlAttrs: {
+      lang: "en"x
+    }
+  })
+
+  definePageMeta({
+    layout: "custom"
+  })
+  
+</script>
+
 <template>
-  <div">Hi from the Events page</div>
+  <div class="Events">Hi from the Events page!!</div>
   <Details />
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+  useHead({
+    title: 'Main',
+    htmlAttrs: {
+      lang: "en"
+    }
+  })
+</script>
+
 <template>
   <div class="bg-[#fab]">
       <h1 class="text-blue-800">Hello Nuxt3 👋</h1>
